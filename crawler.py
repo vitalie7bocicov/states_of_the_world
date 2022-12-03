77cb340237d141088db9b46c53a130ebe426f4b3
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2022-12-03 10:50:56 +0200

    get_capitals feature added

--- crawler.py
+++ crawler.py
@@ -1,14 +1,33 @@
 import requests, re
 from bs4 import BeautifulSoup
 
+def get_capitals(soup):
+    capitals=[]
+    for tr in soup.select('table > tbody > tr'):
+        th = tr.find("th",class_="infobox-label", scope="row")
+        if(re.match(r".*>Capital<.*",str(th))):
+            is_found = False
+            for anchor in tr.td.select("td > a[href]"):
+                capitals.append(anchor.get("title"))
+                is_found = True
+            if not is_found:
+                for anchor in tr.td.select("td > span > a[href]"):
+                    capitals.append(anchor.get("title"))
+                    is_found = True
+            if not is_found:
+                for anchor in tr.td.select("td div ul li > a[href]"):
+                    capitals.append(anchor.get("title"))
+                    is_found = True
+    return capitals
+
 def get_country_data(url):
     base_url = "https://en.wikipedia.org"
     url = base_url + url
     code = requests.get(url)
     plain = code.text
     soup = BeautifulSoup(plain, "html5lib")
-
     country_name = soup.find("h1",id="firstHeading").text
+    capitals=get_capitals(soup)
 
 def crawl(url):
     code = requests.get(url)
